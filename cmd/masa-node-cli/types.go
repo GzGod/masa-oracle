c370b14f2a4f0244049d725e439aba5f9cda593d
Luckyxg <87571231+GzGod@users.noreply.github.com>
2024-11-19 16:11:53 +0800

    Update types.go
    Signed-off-by: Luckyxg <87571231+GzGod@users.noreply.github.com>

--- cmd/masa-node-cli/types.go
+++ cmd/masa-node-cli/types.go
@@ -7,46 +7,46 @@ import (
 	"github.com/rivo/tview"
 )
 
-type AppConfig struct {
-	Address         string
-	Model           string
-	TwitterUser     string
-	TwitterPassword string
-	Twitter2FA      string
+type 应用配置 struct {
+	地址         string
+	模型           string
+	推特用户     string
+	推特密码 string
+	推特2FA      string
 }
 
-var appConfig = AppConfig{}
+var 应用配置实例 = 应用配置{}
 
-var mainFlex *tview.Flex
+var 主Flex *tview.Flex
 
-type Gossip struct {
-	Content  string
-	Metadata map[string]string
+type 八卦 struct {
+	内容  string
+	元数据 map[string]string
 }
 
-type SpeakRequest struct {
-	Text          string `json:"text"`
-	VoiceSettings struct {
-		Stability       float64 `json:"stability"`
-		SimilarityBoost float64 `json:"similarity_boost"`
+type 讲话请求 struct {
+	文本          string `json:"text"`
+	语音设置 struct {
+		稳定性       float64 `json:"stability"`
+		相似度提升 float64 `json:"similarity_boost"`
 	} `json:"voice_settings"`
 }
 
-type SubscriptionHandler struct {
-	Gossips     []Gossip
-	GossipTopic *pubsub.Topic
-	mu          sync.Mutex
+type 订阅处理器 struct {
+	八卦数组     []八卦
+	八卦主题 *pubsub.Topic
+	互斥锁          sync.Mutex
 }
 
-type RadioButtons struct {
+type 单选按钮 struct {
 	*tview.Box
-	options       []string
-	currentOption int
-	onSelect      func(option string)
+	选项       []string
+	当前选项 int
+	选择回调      func(option string)
 }
 
-type InputBox struct {
+type 输入框 struct {
 	*tview.Box
-	input    chan rune
-	textView *tview.TextView
+	输入    chan rune
+	文本视图 *tview.TextView
 }
